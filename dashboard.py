6ce25e75de3a75632461073167d6cdd2d2beb098
Dandi Darmansyah <129208928+dandi462002@users.noreply.github.com>
2026-03-30 01:59:28 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -4,16 +4,22 @@ import seaborn as sns
 import streamlit as st
 import joblib
 import requests
+import io
+
 
 sns.set(style='dark')
 
+@st.cache_resource
 def load_data():
     return pd.read_csv("https://raw.githubusercontent.com/dandi462002/Belajar-Penerapan-Data-Science/main/attrition_dashboard_data.csv")  
 
+@st.cache_resource
 def load_model():
     url = "https://raw.githubusercontent.com/dandi462002/Belajar-Penerapan-Data-Science/main/model_attrition.pkl"
     response = requests.get(url)
     return joblib.load(io.BytesIO(response.content))
+
+
     
 df = load_data()
 model = load_model()
